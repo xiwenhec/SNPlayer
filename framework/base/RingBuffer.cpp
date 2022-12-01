ca27f5a4b1076a835aaffc6c2b4f6a3fe3cda11b
Sivin <xiwenhec@163.com>
2022-12-01 16:18:20 +0800

    重构RingBuffer

--- framework/base/RingBuffer.cpp
+++ framework/base/RingBuffer.cpp
@@ -1,207 +1,150 @@
 
-/*
- *      Copyright (C) 2015 pingkai010@gmail.com
- *
- */
-
 #include "RingBuffer.h"
-#include <cstdlib>
 #include <cstring>
 #include <atomic>
-#include <algorithm>
 #include <cassert>
 
-using namespace std;
+RingBuffer::RingBuffer(uint32_t size) : RingBuffer(size, 0) {}
 
-typedef struct RingBuffer_t {
-    char *m_buffer;
-    unsigned int m_size;
-    unsigned int m_readPtr;
-    unsigned int m_writePtr;
-    atomic<uint32_t> m_fillCount;
-    unsigned int m_back_size;
-    //为了能够回退已经读取过的数据而保留的长度，
-    //写指针不能占用这部分空间
-    atomic<uint32_t> m_backCount;
-} RingBuffer;
+RingBuffer::RingBuffer(uint32_t size, uint32_t backSize) : mSize(size),
+                                                           mBackSize(backSize),
+                                                           mBackCount(0), mReadIndex(0),
+                                                           mWriteIndex(0), mFillCount(0) {
+    mBuffer = new char[size];
+    memset(mBuffer, 0, size);
+}
 
-RingBuffer *RingBufferCreate(uint32_t size) {
-    RingBuffer *rBuf;
-    char *buf = (char *) malloc(size);
-
-    if (!buf) {
-        return nullptr;
+RingBuffer::~RingBuffer() {
+    if (mBuffer != nullptr) {
+        delete[] mBuffer;
+        mBuffer = nullptr;
     }
-
-    rBuf = (RingBuffer *) malloc(sizeof(RingBuffer));
-    memset(rBuf, 0, sizeof(RingBuffer));
-    rBuf->m_size = size;
-    rBuf->m_buffer = buf;
-    rBuf->m_back_size = 0;
-    rBuf->m_backCount = 0;
-    return rBuf;
 }
 
-void RingBufferSetBackSize(RingBuffer *rBuf, uint32_t size) {
-    rBuf->m_back_size = size;
-}
-
-void RingBufferDestroy(RingBuffer *rBuf) {
-    if (rBuf->m_buffer != nullptr) {
-        free(rBuf->m_buffer);
-        rBuf->m_buffer = nullptr;
-    }
-
-    free(rBuf);
-}
-
-void RingBufferClear(RingBuffer *rBuf) {
-    rBuf->m_readPtr = 0;
-    rBuf->m_writePtr = 0;
-    rBuf->m_fillCount = 0;
-    rBuf->m_backCount = 0;
-}
-
-uint32_t RingBufferReadData(RingBuffer *rBuf, char *buf, uint32_t size) {
-    if (size > rBuf->m_fillCount.load()) {
+uint32_t RingBuffer::readData(char *outBuffer, uint32_t wantSize) {
+    if (wantSize > mFillCount.load()) {
         return 0;
     }
 
     //要读取的数据需要回环
-    if (size + rBuf->m_readPtr > rBuf->m_size) {
-        unsigned int chunk = rBuf->m_size - rBuf->m_readPtr;
-        memcpy(buf, rBuf->m_buffer + rBuf->m_readPtr, chunk);
-        memcpy(buf + chunk, rBuf->m_buffer, size - chunk);
-        rBuf->m_readPtr = size - chunk;
-        assert(rBuf->m_readPtr <= rBuf->m_size);
+    if (wantSize + mReadIndex > mSize) {
+        unsigned int chunk = mSize - mReadIndex;
+        memcpy(outBuffer, mBuffer + mReadIndex, chunk);
+        memcpy(outBuffer + chunk, mBuffer, wantSize - chunk);
+        mReadIndex = wantSize - chunk;
+        assert(mReadIndex <= mSize);
     } else {
-        memcpy(buf, rBuf->m_buffer + rBuf->m_readPtr, size);
-        rBuf->m_readPtr += size;
-        assert(rBuf->m_readPtr <= rBuf->m_size);
+        memcpy(outBuffer, mBuffer + mReadIndex, wantSize);
+        mReadIndex += wantSize;
+        assert(mReadIndex <= mSize);
     }
 
-    if (rBuf->m_readPtr == rBuf->m_size) {
-        rBuf->m_readPtr = 0;
+    if (mReadIndex == mSize) {
+        mReadIndex = 0;
     }
 
-    rBuf->m_fillCount -= size;
-    rBuf->m_backCount = std::min(rBuf->m_back_size, rBuf->m_backCount + size);
-    return size;
+    mFillCount -= wantSize;
+    mBackCount = std::min(mBackSize, mBackCount + wantSize);
+
+    return wantSize;
 }
 
-uint32_t RingBufferWriteData(RingBuffer *rBuf, const char *buf, uint32_t size) {
-    if (size > rBuf->m_size - rBuf->m_fillCount.load() - rBuf->m_backCount.load()) {
+
+uint32_t RingBuffer::writeData(const char *inputBuffer, uint32_t inputSize) {
+
+    if (inputSize > mSize - mFillCount.load() - mBackCount.load()) {
         return 0;
     }
 
-    //剩余空间小于要写入的数据大小，需要回环
-    if (size + rBuf->m_writePtr > rBuf->m_size) {
-        //获取写索引到结束位置的空间
-        unsigned int chunk = rBuf->m_size - rBuf->m_writePtr;
-        //拷贝部分数据，从写索引开始位置处写入
-        memcpy(rBuf->m_buffer + rBuf->m_writePtr, buf, chunk);
-        //回环写入
-        memcpy(rBuf->m_buffer, buf + chunk, size - chunk);
-        //更新写索引
-        rBuf->m_writePtr = size - chunk;
+    if (inputSize + mWriteIndex > mSize) {
+        uint32_t chunk = mSize - mWriteIndex;
+        memcpy(mBuffer + mWriteIndex, inputBuffer, chunk);
+        memcpy(mBuffer, inputBuffer + chunk, inputSize - chunk);
+        mWriteIndex = inputSize - chunk;
     } else {
-        if ((rBuf->m_buffer + rBuf->m_writePtr) != buf) {
-            memcpy(rBuf->m_buffer + rBuf->m_writePtr, buf, size);
+        if (mBuffer + mWriteIndex != inputBuffer) {
+            memcpy(mBuffer + mWriteIndex, inputBuffer, inputSize);
         }
-
-        rBuf->m_writePtr += size;
+        mWriteIndex += inputSize;
     }
 
-    if (rBuf->m_writePtr == rBuf->m_size) {
-        rBuf->m_writePtr = 0;
+    if (mWriteIndex == mSize) {
+        mWriteIndex = 0;
     }
 
-    rBuf->m_fillCount += size;
-    return size;
+    mFillCount += inputSize;
+    return inputSize;
 }
 
-int64_t RingBufferSkipBytes(RingBuffer *rBuf, int64_t skipSize) {
-    int64_t size;
+void RingBuffer::clear() {
+    mReadIndex = 0;
+    mWriteIndex = 0;
+    mFillCount = 0;
+    mBackCount = 0;
+}
 
-    if (skipSize < 0) {
-        size = -skipSize;
+int64_t RingBuffer::skipBytes(int64_t skpSize) {
+
+    int64_t skipSize = 0;
+
+    if (skpSize < 0) {
+        skipSize = -skpSize;
         //回退的长度小于(读后缓冲 backCount)长度，表示可以直接缓冲回退，不会污染写空间
-        if (rBuf->m_backCount.load() >= size) {
-            //更新回退缓冲大小
-            rBuf->m_backCount -= size;
-            //当前读指针的位置小于要回退的大小，则回环
-            if (rBuf->m_readPtr < size) {
-                //回环处理 回退读指针
-                rBuf->m_readPtr = rBuf->m_size - (size - rBuf->m_readPtr);
-                assert(rBuf->m_readPtr <= rBuf->m_size);
-            } else {
-                //直接回推读指针
-                rBuf->m_readPtr -= size;
-                assert(rBuf->m_readPtr <= rBuf->m_size);
+        if (mBackCount.load() >= skipSize) {
+            mBackCount -= skipSize;
+            //当前读指针的位置小于要回推的大小，则需要回环回退
+            if (mReadIndex < skipSize) {
+                mReadIndex = mSize - (skipSize - mReadIndex);
+                assert(mReadIndex <= mSize);
+            } else { //直接回退指针
+                mReadIndex -= skipSize;
+                assert(mReadIndex <= mSize);
             }
-
-            if (rBuf->m_readPtr == rBuf->m_size) {
-                rBuf->m_readPtr = 0;
+            if (mReadIndex == mSize) {
+                mReadIndex = 0;
             }
             //更新可读数据大小
-            rBuf->m_fillCount += size;
+            mFillCount += skipSize;
             return skipSize;
         }
-
+        //不能回退
         return 0;
     }
-
-    size = skipSize;
-
+    skipSize = skpSize;
     //向前seek，超过了可读空间大小，直接返回
-    if (size > rBuf->m_fillCount) {
+    if (skipSize > mFillCount) {
         return 0;
     }
 
-
-    if (size + rBuf->m_readPtr > rBuf->m_size) {
-        //回环前进，更新读指针
-        unsigned int chunk = rBuf->m_size - rBuf->m_readPtr;
-        rBuf->m_readPtr = size - chunk;
-        assert(rBuf->m_readPtr <= rBuf->m_size);
+    if (skipSize + mReadIndex > mSize) {
+        //前进需要回环
+        uint64_t chunk = mSize - mReadIndex;
+        mReadIndex = skipSize - chunk;
+        assert(mReadIndex <= mSize);
     } else {
         //直接更新读指针
-        rBuf->m_readPtr += size;
-        assert(rBuf->m_readPtr <= rBuf->m_size);
+        mReadIndex += skipSize;
+        assert(mReadIndex <= mSize);
     }
-
-    if (rBuf->m_readPtr == rBuf->m_size) {
-        rBuf->m_readPtr = 0;
+    if (mReadIndex == mSize) {
+        mReadIndex = 0;
     }
-    //更新可读空间大小
-    rBuf->m_fillCount -= size;
+    mFillCount -= skipSize;
     return skipSize;
 }
 
-char *RingBufferGetBuffer(RingBuffer *rBuf) {
-    return rBuf->m_buffer;
+uint64_t RingBuffer::getMaxReadableDataSize() const {
+    return mFillCount.load();
 }
 
-uint32_t RingBufferGetSize(RingBuffer *rBuf) {
-    return rBuf->m_size;
+uint64_t RingBuffer::getMaxWriteableDataSize() const {
+    return mSize - mFillCount.load() - mBackCount.load();;
 }
 
-unsigned int RingBufferGetReadPtr(RingBuffer *rBuf) {
-    return rBuf->m_readPtr;
+uint64_t RingBuffer::getMaxBackSize() const {
+    return mBackCount;
 }
 
-unsigned int RingBufferGetWritePtr(RingBuffer *rBuf) {
-    return rBuf->m_writePtr;
-}
 
-uint32_t RingBufferGetAvailableReadSize(RingBuffer *rBuf) {
-    return rBuf->m_fillCount.load();
-}
 
-uint32_t RingBufferGetAvailableWriteSize(RingBuffer *rBuf) {
-    return rBuf->m_size - rBuf->m_fillCount.load() - rBuf->m_backCount.load();
-}
 
-uint32_t RingBufferGetAvailableBackSize(RingBuffer *rBuf) {
-    return rBuf->m_backCount;
-}
--- framework/base/RingBuffer.h
+++ framework/base/RingBuffer.h
@@ -1,48 +1,83 @@
 
-/*
- *      Copyright (C) 2015 pingkai010@gmail.com
- *
- */
-#ifndef RING_BUFFER_H
-#define RING_BUFFER_H
+#ifndef SIVINPLAYER_RINGBUFFER_H
+#define SIVINPLAYER_RINGBUFFER_H
 
 #include <cstdint>
+#include <atomic>
+#include <memory>
 
-#ifdef __cplusplus
-extern "C" {
+//#ifdef __cplusplus
+//extern "C" {
+//#endif
+//struct RingBuffer_t;
+//
+//RingBuffer_t *RingBufferCreate(uint32_t size);
+//
+//void RingBufferSetBackSize(RingBuffer_t *rBuf, uint32_t size);
+//
+//void RingBufferDestroy(RingBuffer_t *rBuf);
+//
+//void RingBufferClear(RingBuffer_t *rBuf);
+//
+//uint32_t RingBufferReadData(RingBuffer_t *rBuf, char *buf, uint32_t size);
+//
+//uint32_t RingBufferWriteData(RingBuffer_t *rBuf, const char *buf, uint32_t size);
+//
+//int64_t RingBufferSkipBytes(RingBuffer_t *rBuf, int64_t skipSize);
+//
+//char *RingBufferGetBuffer(RingBuffer_t *rBuf);
+//
+//uint32_t RingBufferGetSize(RingBuffer_t *rBuf);
+//
+//unsigned int RingBufferGetReadPtr(RingBuffer_t *rBuf);
+//
+//unsigned int RingBufferGetWritePtr(RingBuffer_t *rBuf);
+//
+//uint32_t RingBufferGetAvailableReadSize(RingBuffer_t *rBuf);
+//
+//uint32_t RingBufferGetAvailableWriteSize(RingBuffer_t *rBuf);
+//uint32_t RingBufferGetAvailableBackSize(RingBuffer_t *rBuf);
+//
+//#ifdef __cplusplus
+//}
+//#endif
+//
+
+
+class RingBuffer {
+public:
+    explicit RingBuffer(uint32_t size);
+
+    RingBuffer(uint32_t size, uint32_t backSize);
+
+    ~RingBuffer();
+
+    void setBackSize(uint32_t size) { mBackSize = size; }
+
+    uint32_t readData(char *outBuffer, uint32_t size);
+
+    uint32_t writeData(const char *inputBuffer, uint32_t size);
+
+    int64_t skipBytes(int64_t skipSize);
+
+    uint64_t getMaxReadableDataSize() const;
+
+    uint64_t getMaxWriteableDataSize() const;
+
+    uint64_t getMaxBackSize() const;
+
+    void clear();
+
+private:
+    char *mBuffer;
+    uint64_t mSize;
+    uint64_t mBackSize;
+    unsigned int mReadIndex;
+    unsigned int mWriteIndex;
+    std::atomic<uint64_t> mFillCount;
+    //为了能够回退已经读取过的数据而保留的长度，
+    //写指针不能占用这部分空间
+    std::atomic<uint64_t> mBackCount;
+};
+
 #endif
-typedef struct RingBuffer_t RingBuffer;
-
-RingBuffer *RingBufferCreate(uint32_t size);
-
-void RingBufferSetBackSize(RingBuffer *rBuf,uint32_t size);
-
-void RingBufferDestroy(RingBuffer *rBuf);
-
-void RingBufferClear(RingBuffer *rBuf);
-
-uint32_t RingBufferReadData(RingBuffer *rBuf, char *buf, uint32_t size);
-
-uint32_t RingBufferWriteData(RingBuffer *rBuf, const char *buf, uint32_t size);
-
-int64_t RingBufferSkipBytes(RingBuffer *rBuf, int64_t skipSize);
-
-char *RingBufferGetBuffer(RingBuffer *rBuf);
-
-uint32_t RingBufferGetSize(RingBuffer *rBuf);
-
-unsigned int RingBufferGetReadPtr(RingBuffer *rBuf);
-
-unsigned int RingBufferGetWritePtr(RingBuffer *rBuf);
-
-uint32_t RingBufferGetAvailableReadSize(RingBuffer *rBuf);
-
-uint32_t RingBufferGetAvailableWriteSize(RingBuffer *rBuf);
-uint32_t RingBufferGetAvailableBackSize(RingBuffer *rBuf);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
-
