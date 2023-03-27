3c56c8df8f7158b852cb1fb3f1abcdbb9ff60ce3
sivin <xiwenhec@163.com>
2023-03-27 20:20:42 +0800

    定义解码器

--- framework/codec/IDecoder.h
+++ framework/codec/IDecoder.h
@@ -5,10 +5,17 @@
 #ifndef DATASOURCETEST_ICODEC_H
 #define DATASOURCETEST_ICODEC_H
 
+#include <cstdint>
 namespace Sivin {
   class IDecoder {
+    explicit IDecoder() = default;
+    virtual ~IDecoder() = default;
 
-    
+  public:
+    virtual uint32_t getInputQueueSize() = 0;
+
+
+
   };
 }// namespace Sivin
 
